ba69d3ff7dcda91e0ffa62df3ec97ad2ae9580f2
Andrew Pryshchepa <vinglfm@gmail.com>
2015-12-27 15:59:02 +0200

    Assignment 3

--- AngularJS/conFusion-gulp/app/scripts/app.js
+++ AngularJS/conFusion-gulp/app/scripts/app.js
@@ -2,76 +2,70 @@
 
 angular.module('confusionApp', ['ui.router'])
 .config(function($stateProvider, $urlRouterProvider) {
-          $stateProvider.state('app', {
-              url:'/',
-              views: {
-                'header': {
-                  templateUrl:'views/header.html',
-                },
-                'content': {
-                  template : '<h3>Placeholder</h3>',
-                  controller: 'IndexController',
-                },
-                'footer': {
-                  templateUrl: 'views/footer.html',
+        $stateProvider
+        
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/home.html',
+                        controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html',
+                    }
                 }
-              }
-          })
-          .state('app.aboutus', {
-            url:'aboutus',
-            views: {
-              'content@': {
-                template : '<h3>Placeholder</h3>',
-                controller : 'AboutController'
-              }
-            }
-          })
-          .state('app.contactus', {
-            url:'contactus',
-            view: {
-              'content@': {
-                templateUrl : 'views/contactus.html',
-                controller : 'ContactController'
-              }
-            }
-          })
-          .state('app.menu', {
-            url: 'menu',
-            views: {
-              'content@': {
-                templateUrl : 'views/menu.html',
-                controller  : 'MenuController'
-              }
-            }
-          })
-          .state('app.dishdetails', {
-              url: 'menu/:id',
-              views: {
-                'content@': {
-                  templateUrl : 'views/dishdetail.html',
-                  controller  : 'DishDetailController'
-                 }
-              }
-          });
-          $urlRouterProvider.otherwise('/');
-});
-// angular.module('confusionApp', ['ngRoute'])
-// .config(function($routeProvider) {
-//         $routeProvider
-//             // route for the contactus page
-//             .when('/contactus', {
-//                 templateUrl : 'contactus.html',
-//                 controller  : 'ContactController'
-//             })
-//             // route for the menu page
-//             .when('/menu', {
-//                 templateUrl : 'menu.html',
-//                 controller  : 'MenuController'
-//             })
-//             // route for the dish details page
-//             .when('/menu/:id', {
-//                 templateUrl : 'dishdetail.html',
-//                 controller  : 'DishDetailController'
-//             })
-//             .otherwise('/contactus');
-//     });
+
+            })
+        
+            // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/aboutus.html',
+                        controller  : 'AboutController'                  
+                    }
+                }
+            })
+        
+            // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'                  
+                    }
+                }
+            })
+
+            // route for the menu page
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/menu.html',
+                        controller  : 'MenuController'
+                    }
+                }
+            })
+
+            // route for the dishdetail page
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/dishdetail.html',
+                        controller  : 'DishDetailController'
+                   }
+                }
+            });
+    
+        $urlRouterProvider.otherwise('/');
+    })
+;
--- AngularJS/conFusion-gulp/app/scripts/controllers.js
+++ AngularJS/conFusion-gulp/app/scripts/controllers.js
@@ -2,18 +2,18 @@
 
 angular.module('confusionApp')
 
-        .controller('MenuController', ['$scope', 'menuFactory',
-        function($scope, menuFactory) {
-
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes = menuFactory.getDishes();
+            $scope.dishes= menuFactory.getDishes();
 
+                        
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
-
+                
                 if (setTab === 2) {
                     $scope.filtText = "appetizer";
                 }
@@ -31,49 +31,69 @@ angular.module('confusionApp')
             $scope.isSelected = function (checkTab) {
                 return ($scope.tab === checkTab);
             };
-
+    
             $scope.toggleDetails = function() {
                 $scope.showDetails = !$scope.showDetails;
             };
         }])
-          .controller('ContactController', ['$scope', function($scope) {
-                $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-                $scope.channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-                $scope.invalidChannelSelection = false;
-          }])
 
-          .controller('FeedbackController', ['$scope', function($scope) {
-                $scope.sendFeedback = function() {
-                  console.log($scope.feedback);
-                  if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
-                      $scope.invalidChannelSelection = true;
-                      console.log('incorrect');
-                  }
-                  else {
-                      $scope.invalidChannelSelection = false;
-                      $scope.feedback = {mychannel:"", firstName:"", lastName:"",
-                        agree:false, email:"" };
-                      $scope.feedback.mychannel="";
+        .controller('ContactController', ['$scope', function($scope) {
 
-                      $scope.feedbackForm.$setPristine();
-                      console.log($scope.feedback);
-                    }
-                };
-            }])
-
-        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory',
-        function($scope, $stateParams, menuFactory) {
-            $scope.dish = menuFactory.getDish(parseInt($stateParams.id, 10));
-            $scope.order = '';
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
         }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
+            
+            $scope.dish = dish;
+            
+        }])
+
         .controller('DishCommentController', ['$scope', function($scope) {
-            $scope.comment = {author:"", rating:"5", comment:"", date:""};
-
+            
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            
             $scope.submitComment = function () {
-                $scope.comment.date = new Date().toISOString();
-                $scope.dish.comments.push($scope.comment);
-
+                
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
+                
+                $scope.dish.comments.push($scope.mycomment);
+                
                 $scope.commentForm.$setPristine();
-                $scope.comment = {author:"", rating:"5", comment:""};
+                
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             }
-        }]);
+        }])
+
+        // implement the IndexController and About Controller here
+
+
+;
