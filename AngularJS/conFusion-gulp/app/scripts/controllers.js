8e01196e8da1cc27f6e943bd58f70f68ae6af1a6
Andrew Pryshchepa <vinglfm@gmail.com>
2015-12-27 15:47:52 +0200

    ngView samples

---
+++ AngularJS/conFusion-gulp/app/scripts/controllers.js
@@ -0,0 +1,79 @@
+'use strict';
+
+angular.module('confusionApp')
+
+        .controller('MenuController', ['$scope', 'menuFactory',
+        function($scope, menuFactory) {
+
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+
+            $scope.dishes = menuFactory.getDishes();
+
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+          .controller('ContactController', ['$scope', function($scope) {
+                $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                $scope.channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+                $scope.invalidChannelSelection = false;
+          }])
+
+          .controller('FeedbackController', ['$scope', function($scope) {
+                $scope.sendFeedback = function() {
+                  console.log($scope.feedback);
+                  if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                      $scope.invalidChannelSelection = true;
+                      console.log('incorrect');
+                  }
+                  else {
+                      $scope.invalidChannelSelection = false;
+                      $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                        agree:false, email:"" };
+                      $scope.feedback.mychannel="";
+
+                      $scope.feedbackForm.$setPristine();
+                      console.log($scope.feedback);
+                    }
+                };
+            }])
+
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory',
+        function($scope, $stateParams, menuFactory) {
+            $scope.dish = menuFactory.getDish(parseInt($stateParams.id, 10));
+            $scope.order = '';
+        }])
+        .controller('DishCommentController', ['$scope', function($scope) {
+            $scope.comment = {author:"", rating:"5", comment:"", date:""};
+
+            $scope.submitComment = function () {
+                $scope.comment.date = new Date().toISOString();
+                $scope.dish.comments.push($scope.comment);
+
+                $scope.commentForm.$setPristine();
+                $scope.comment = {author:"", rating:"5", comment:""};
+            }
+        }]);
--- AngularJS/conFusion-gulp/gulpfile.js
+++ AngularJS/conFusion-gulp/gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/**/*.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
@@ -76,8 +76,9 @@ gulp.task('browser-sync', ['default'], function () {
    browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
-      }
+         index: "index.html"
+      },
+      reloadDelay: 1000
    });
         // Watch any files in dist/, reload on change
   gulp.watch(['dist/**']).on('change', browserSync.reload);
